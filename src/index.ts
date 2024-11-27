4899d850b8b2fa509212156274338ab8d57e6a3b
TESTIMONY <mac@MACs-MacBook-Air.local>
2024-11-27 04:01:12 +0100

    added a welcoming message to the root route

--- src/index.ts
+++ src/index.ts
@@ -27,6 +27,18 @@ app.use(cors());
 
 app.use("/api/v1/users", userRouter);
 
+app.get("/", (req, res) => {
+  res.send(`
+      WELCOME TO THE EVENT CREATION APP!<br><br>
+      This platform allows users to effortlessly create and manage events, while also providing an easy way for attendees to purchase tickets. 
+      Whether you are hosting a concert, conference, or party, our app makes the event creation process simple and streamlined. 
+      Users can create events, customize event details, and start selling tickets within minutes. 
+      Attendees can browse through upcoming events, choose the tickets they want, and make secure purchases all in one place. 
+      We aim to make event management and ticket purchasing accessible and efficient for everyone. 
+      Get started now and experience the convenience of organizing your next event with us!
+  `);
+});
+
 try {
   app.listen(port, () => {
     console.log(`Server running on ${URL}:${port}`);

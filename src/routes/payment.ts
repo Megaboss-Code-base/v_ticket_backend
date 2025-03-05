a84a7cd534964df665001abd5c172a03ea21b992
TESTIMONY <mac@MACs-MacBook-Air.local>
2025-03-05 13:24:13 +0100

    removed stripe

--- src/routes/payment.ts
+++ src/routes/payment.ts
@@ -1,17 +1,13 @@
 import express from "express";
 import {
-  confirmStripePayment,
   handlePaymentVerification,
   handleUnifiedWebhook,
   purchaseTicket,
-  stripePayment,
 } from "../controllers/paymentCtrl";
 
 const router = express.Router();
 
 router.post("/create-payment-link/:eventId", purchaseTicket);
-router.post("/create-stripe-link/:eventId", stripePayment);
-router.get("/confirm-stripe-payment", confirmStripePayment);
 router.post("/webhook", handleUnifiedWebhook);
 router.post("/verify", handlePaymentVerification);
 

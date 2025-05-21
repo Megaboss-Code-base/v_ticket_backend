4e46dcf7b9da718d4e3d8e1f4a0aed99a2e9c134
TESTIMONY <mac@MACs-MacBook-Air.local>
2025-05-21 19:41:13 +0100

    testing paystack

--- src/controllers/paymentCtrl.ts
+++ src/controllers/paymentCtrl.ts
@@ -44,9 +44,7 @@ const getCustomFieldValue = (
   return field ? field.value : "";
 };
 
-console.log("PAYSTACK_BASE_URL",PAYSTACK_BASE_URL)
 console.log("PAYSTACK_SECRET_KEY",PAYSTACK_SECRET_KEY)
-console.log("111",)
 
 export const purchaseTicket = async (
   req: Request,
